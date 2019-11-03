16ccd14ad786b078c6f11663bc2afea38dfee596
Abhishek Bhatt <46929125+Abhishek-1Bhatt@users.noreply.github.com>
2019-11-03 06:13:16 +0530

    Fully Functional
    Rules described at the beginning, provided condition for the first guess being out of range of 10 from the lucky guess with an else condition.

--- guessingGame.py
+++ guessingGame.py
@@ -1,28 +1,27 @@
-count=0
-guess=0
-inp=int(input('Enter your guess: '))
-pre=inp
+from random import randint
+lg=randint(1,100)
+print("The rules of the game are:\n-If a player's guess is less than 1 or greater than 100,they will have to enter another guess after the message 'OUT OF BOUNDS'\n-On a player's first turn, if their guess is:\n\t-within 10 of the number, the game will return the message 'WARM!'\n\t-further than 10 away from the number,'COLD!'\n\t-If its not within a range of 10 they will see the message 'Nowhere close!'\n-On all subsequent turns, if a guess is\n\t-closer to the number than the previous guess its 'WARMER!'\n\t-farther from the number than the previous guess,its 'COLDER!'\n-When the player's guess equals the number, it tells them they've guessed correctly and how many guesses it took!")
+count=1
+inp=int(input('Enter no.: '))
 while inp!=lg:
-    if count==0:
-        if inp<= lg+10 and inp>=lg-10:
-            print('Warm!')
+   if inp<1 or inp>100:
+    inp=int(input('Invalid Input:'))
+   else:
+    if count==1:
+        predif=abs(inp-lg)
+        count+=1
+        if inp in range(lg-10,lg):
+            inp=int(input('WARM!'))
+        elif inp in range(lg,lg+10):
+            inp=int(input('COLD'))
         else:
-            print('Cold!')
-        count = count+1
-        continue
-    count=count+1
-    inp=int(input('Guess Again :) '))
-    if inp==lg:
-        print(f'Hey! you guessed it right after {count} attempts')
-    elif inp>lg and pre>lg:
-        if inp-lg<pre-lg:
-            print('Warmer!')
-        elif inp-lg>pre-lg:
-            print('Colder!')
-    elif inp<lg and pre<lg:
-        if lg-pre>lg-inp:
-            print('Warmer!')
-        elif lg-pre<lg-inp:
-            print('Colder!')
-if count==0:
-    print('Congrats, guessed it right in the first attempt!')
+            inp=int(input('Nowhere close!'))
+    else:
+        count+=1
+        if abs(inp-lg)<predif:
+            predif=abs(inp-lg)
+            inp=int(input('WARMER!'))
+        elif abs(inp-lg)>predif:
+            predif=abs(inp-lg)
+            inp=int(input('COLDER!'))
+print(f'It took {count} attempts to figure out the number')
